ac0d13fe964802bff2036a0aa49b12e1983f844a
Maqa085 <mako38848@gmail.com>
2026-04-09 01:17:27 +0400

    Add API_ID and API_HASH to config
    Add API_ID and API_HASH configuration from environment variables.

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 from os import getenv
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
+API_ID = int(getenv("API_ID", "0")) # .env-ə əlavə et
+API_HASH = getenv("API_HASH")       # .env-ə əlavə et
 BOT_TOKEN = getenv("BOT_TOKEN")
